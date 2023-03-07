696443e7486d60903c0119730d088ac3b40cd92b
MI KE <fernandesmandrew@gmail.com>
2023-03-07 10:23:01 +0800

    inital commit

---
+++ LibrarySystem/ConfirmBorrow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibrarySystem
+{
+    public partial class ConfirmBorrow : Form
+    {
+        public ConfirmBorrow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmBorrow_Load(object sender, EventArgs e)
+        {
+            // Get the currently selected row
+            lblBookTitle.Text = HomeScreen.title;
+            lblAuthor.Text = "by " + HomeScreen.author;
+        }
+
+        private void BtnClose_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
